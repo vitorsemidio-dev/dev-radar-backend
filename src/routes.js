1adbbd146dd141c2f27e5eaf53e1ee91377b8d1b
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-01-16 20:38:18 -0300

    DevSchema, Buscando dados github e salvando no banco

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,7 @@
 const { Router } = require('express');
+const axios = require('axios');
+
+const Dev = require('./models/Dev');
 
 const routes = Router();
 
@@ -6,6 +9,27 @@ routes.get('/', (req, res) => {
 
   return res.json({ ok: true });
 });
+
+routes.post('/devs', async (req, res) => {
+  const { github_username, techs } = req.body;
+
+  const response = await axios.get(`https://api.github.com/users/${github_username}`);
+
+  const { name = login, avatar_url, bio = '' } = response.data;
+  const techsArray = techs.split(',').map(tech => tech.trim());
+
+  console.log({ name, avatar_url, bio, techsArray });
+
+  const dev = await Dev.create({
+    github_username,
+    name,
+    avatar_url,
+    bio,
+    techs: techsArray,
+  });
+
+  return res.json(dev);
+})
 
 
 module.exports = routes;

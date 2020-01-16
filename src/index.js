609518500804ebfe8556f8bba411f1380a2f7375
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-01-16 20:37:08 -0300

    Fix: Conexão banco de dados via docker

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ require('dotenv/config');
 const express = require('express');
 const mongoose = require('mongoose');
 
-mongoose.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_USER}@devradar-zhpu3.mongodb.net/${process.env.MONGODB_NAME}?retryWrites=true&w=majority`, {
+mongoose.connect(`${process.env.MONGODB_HOST}`, {
   useNewUrlParser: true,
+  useFindAndModify: true,
   useUnifiedTopology: true,
 });
 

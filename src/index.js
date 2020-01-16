bd5bc99d71e772a8845fab41be48a52dbdb791ad
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-01-15 22:04:54 -0300

    Conexão com banco de dados

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
+require('dotenv/config');
+
 const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_USER}@devradar-zhpu3.mongodb.net/${process.env.MONGODB_NAME}?retryWrites=true&w=majority`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const routes = require('./routes')
 

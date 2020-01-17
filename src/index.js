7fe776e2c5c182c910c07e30511f7b8066a0bd9c
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-01-17 00:39:52 -0300

    Cors habilitado

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require('dotenv/config');
 
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 mongoose.connect(`${process.env.MONGODB_HOST}`, {
   useNewUrlParser: true,
@@ -13,6 +14,7 @@ const routes = require('./routes')
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use(routes);
